ef7f38f6e080f1e7aeaa866d21a5656697bd0f48
MateKurtsikdize <126270030+MateKurtsikdize@users.noreply.github.com>
2023-04-01 21:45:02 +0400

    finished (did some debugging)

--- jss/script.js
+++ jss/script.js
@@ -1,21 +1,16 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-  
-    burger.addEventListener('click', () => {
-      nav.classList.toggle('nav-active');
-      burger.classList.toggle('active');
-      navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = '';
-        } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-        }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        document.querySelector(`.burger`).addEventListener('click', () => {
+        document.querySelector('.nav-links').classList.toggle('nav-active');
+        document.querySelector(`.burger`).classList.toggle('active');
+        document.querySelectorAll('.nav-links li').forEach((link, index) => {
+          if (link.style.animation) {
+            link.style.animation = '';
+          } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+          }
+        });
+    
+        
       });
-  
-      
     });
-  };
-  
-  navSlide();
